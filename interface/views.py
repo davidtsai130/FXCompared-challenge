cc8399d12f9bb2d55cf51e3f79641814cca05f25
davidtsai130 <davidtsai130@gmail.com>
2016-09-13 21:56:29 -0400

    refactored code for views

--- interface/views.py
+++ interface/views.py
@@ -1,12 +1,15 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Account
 # Create your views here.
 
 def index(request):
     all_accounts = Account.objects.all()
-    context = {'all_accounts': all_accounts}
-    return render(request, 'interface/index.html', context)
+    return render(request, 'interface/index.html', {'all_accounts': all_accounts})
 
 def show(request, account_id):
-    return HttpResponse("<h2>Details for Account ID: " + str(account_id) + "</h2>")
+    try:
+        account = Account.objects.get(pk=account_id)
+    except Account.DoesNotExist:
+        raise Http404("Account does not exist")
+    return render(request, 'interface/show.html', {'account': account})
