0180d461ed5851b86fddfa9c3828671e34dc0e04
davidtsai130 <davidtsai130@gmail.com>
2016-09-13 21:39:35 -0400

    switched index rendering from httpresponse to render, and fixed links on index page

--- interface/views.py
+++ interface/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Account
 # Create your views here.
 
 def index(request):
     all_accounts = Account.objects.all()
-    template = loader.get_template('interface/index.html')
-    context = {
-        'all_accounts': all_accounts,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'all_accounts': all_accounts}
+    return render(request, 'interface/index.html', context)
 
 def show(request, account_id):
     return HttpResponse("<h2>Details for Account ID: " + str(account_id) + "</h2>")
